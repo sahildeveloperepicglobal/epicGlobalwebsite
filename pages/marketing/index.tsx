954203c225ececc4e68a40e993411d3695079b04
SahilMishraDev <msahil76640@gmail.com>
2022-10-17 16:01:21 +0530

    changes

---
+++ pages/marketing/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "../../styles/otherstyle/market.scss";
+
+function Marketing() {
+  return <div className={Style.container}>Marketing</div>;
+}
+
+export default Marketing;
